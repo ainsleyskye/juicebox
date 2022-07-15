543e38fbfb948ac7bd28b3b5282cdcf5ac42ba12
ainsleyskye <ndboyd95@gmail.com>
2022-07-15 14:15:59 -0600

    the instructions lied to us. the audacity.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-const PORT = 3000;
+const { PORT = 3000 } = process.env;
 const express = require("express");
 const server = express();
 
@@ -12,10 +12,12 @@ server.use(express.json());
 const apiRouter = require("./api");
 server.use("/api", apiRouter);
 
-server.get('/add/:first/to/:second', (req, res, next) => {
-  res.send(`<h1>${ req.params.first } + ${ req.params.second } = ${
-    Number(req.params.first) + Number(req.params.second)
-   }</h1>`);
+server.get("/add/:first/to/:second", (req, res, next) => {
+  res.send(
+    `<h1>${req.params.first} + ${req.params.second} = ${
+      Number(req.params.first) + Number(req.params.second)
+    }</h1>`
+  );
 });
 
 server.listen(PORT, () => {
